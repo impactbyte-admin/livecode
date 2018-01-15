aaeb372b5361696a21d100aa50ab84a84437bc1f
M Haidar Hanif <git@mhaidarhanif.com>
2018-01-15 16:10:33 +0700

    Initial live code condition

---
+++ people.sh
@@ -0,0 +1,11 @@
+1. Aditia Darmadi Jiwandono
+2. Albert Fajar Yuga Yusdi Putra
+3. Angeline Go
+4. Ferryal Muhamad Fajar Bahari
+5. Fikri Muhamad
+6. Miftahul Agtamas Fidyawan
+7. Muhammad Fahri Abdul Hakim
+8. Muhammad Haekal
+9. Muhammad Haidar Dzaky
+10. Nina Kartina
+11. Oliver Audhie
